237a02ad23a71096a3cd4868941f4e1b66c2d0b9
Joonas Nivala <joonas.nivala@gmail.com>
2026-03-20 16:57:16 +0200

    top level imports

--- src/fiqci/ems/__init__.py
+++ src/fiqci/ems/__init__.py
@@ -2,6 +2,8 @@
 
 from fiqci.ems.fiqci_backend import FiQCIBackend
 from fiqci.ems.rem import M3IQM
+from fiqci.ems.fiqci_estimator import FiQCIEstimator
+from fiqci.ems.fiqci_sampler import FiQCISampler
 
 
-__all__ = ["FiQCIBackend", "M3IQM"]
+__all__ = ["FiQCIBackend", "M3IQM", "FiQCIEstimator", "FiQCISampler"]
