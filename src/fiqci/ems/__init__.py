3623a68033aa440613a52293396357b48bc71514
Joonas Nivala <joonas.nivala@gmail.com>
2026-03-31 14:37:23 +0300

    edit import

--- src/fiqci/ems/__init__.py
+++ src/fiqci/ems/__init__.py
@@ -1,8 +1,7 @@
 """"""
 
 from fiqci.ems.fiqci_backend import FiQCIBackend
-from fiqci.ems.mitigators.rem import M3IQM
 from fiqci.ems.primitives import FiQCIEstimator
 from fiqci.ems.primitives import FiQCISampler
 
-__all__ = ["FiQCISampler", "FiQCIEstimator", "FiQCIBackend", "M3IQM"]
+__all__ = ["FiQCISampler", "FiQCIEstimator", "FiQCIBackend"]
