3a12ae8fd564d31c4a617256f6f7018798cb8890
Joonas Nivala <joonas.nivala@gmail.com>
2026-03-31 16:30:49 +0300

    remove unused

--- src/fiqci/ems/mitigators/__init__.py
+++ src/fiqci/ems/mitigators/__init__.py
@@ -1,8 +1,3 @@
 """
 Mitigators for FiQCI EMS
 """
-
-from . import zne
-from . import rem
-
-__all__ = ["zne", "rem"]
--- src/fiqci/ems/transpiler_passes/__init__.py
+++ src/fiqci/ems/transpiler_passes/__init__.py
@@ -1,8 +1,3 @@
 """
 Transpiler passes for FiQCI EMS
 """
-
-from . import basis_measurement
-from . import zne_circuits
-
-__all__ = ["basis_measurement", "zne_circuits"]
