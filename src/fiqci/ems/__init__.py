7873a938486489f5dbc1e69463885913a9aa5d15
Joonas Nivala <joonas.nivala@gmail.com>
2026-06-04 16:47:43 +0300

    export BatchFailedError from fiqci.ems

--- src/fiqci/ems/__init__.py
+++ src/fiqci/ems/__init__.py
@@ -2,8 +2,8 @@
 FiQCI Error Mitigation Service (EMS).
 """
 
-from fiqci.ems.fiqci_backend import FiQCIBackend
+from fiqci.ems.fiqci_backend import BatchFailedError, FiQCIBackend
 from fiqci.ems.primitives import FiQCIEstimator
 from fiqci.ems.primitives import FiQCISampler
 
-__all__ = ["FiQCISampler", "FiQCIEstimator", "FiQCIBackend"]
+__all__ = ["FiQCISampler", "FiQCIEstimator", "FiQCIBackend", "BatchFailedError"]
